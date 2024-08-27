42b9db0d604ae5f2e9b289953b42edebd6c06708
Vkscoma <vkscoma@gmail.com>
2024-08-27 16:01:18 -0700

    Pushing updated Nav Comp

--- Real-Estate-Project/app/layout.tsx
+++ Real-Estate-Project/app/layout.tsx
@@ -1,23 +1,22 @@
-import Navbar from '@/components/Navbar'
-import '@/assets/styles/global.css'
+import Navbar from "@/components/Navbar";
+import "@/assets/styles/global.css";
 
 export const metadata = {
-    title: 'Property Finder',
-    keywords: 'property, management, real estate',
-    description: 'Main layout for the property management system',
-}
+  title: "Property Finder",
+  keywords: "property, management, real estate",
+  description: "Main layout for the property management system",
+};
 
-const MainLayout = ({children}: {children: React.ReactNode}) => {
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html>
-        <body>
-            <Navbar />
-            <main>
-                {children}
-            </main>
-        </body>
+    <html lang="en">
+      <head></head>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
